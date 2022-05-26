2f9a94a0d75805691e64204fa999d86a210745b6
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-05-26 16:07:48 +0200

    add searchParams

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './sass/main.scss';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const searchParams = new URLSearchParams({
+    key: '27669680-6a77b114183598540616a731d',
+    q: 'dog',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    
+  });
+
+  function fetchSerch() {
+    return fetch(`${BASE_URL}?${searchParams}`).then(response => {
+     if(response.ok) {
+           return response.json();
+     }
+      throw new Error('Error fetching data')
+    });
+  }
+
+fetchSerch();
+
